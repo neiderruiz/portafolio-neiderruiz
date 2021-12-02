48724391de4f4d232286598e8a4e029e2150890a
neiderbtc <neiderbtc@gmail.com>
2021-12-01 21:28:38 -0500

    extend color purple and add background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge:  ["./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        purple: {
+          600: '#BB6BD9',
+          700: '#A042C2',
+        },
+        background: {
+          DEFAULT: '#F9FAFF',
+        }
+      },
+
+    },
   },
   variants: {
     extend: {},
