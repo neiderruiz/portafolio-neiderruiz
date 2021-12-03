7629691675fd42a057942ef299dbd8e88589feef
neiderbtc <neiderbtc@gmail.com>
2021-12-03 09:45:15 -0500

    add section projects, information of contact, modal view more page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,38 +1,75 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import Container from '../containers/Container';
 import HeroHome from '../heros/HeroHome';
 import TitleSection from '../titles/TitleSection';
-import Akunaipa from '../../assets/images/akunaipa-web.png';
-import LandingPageOne from '../../assets/images/landing-page-one.png';
 import Card from '../cards/Card';
+import { get } from '../../services/api/api';
+import ModalMorePage from '../modals/ModalMorePage';
+import CardContact from '../cards/CardContact';
+import { informationContact, informationHome } from '../../data/aboutMe';
 
-
-const projects = [
-    {
-        title: 'Akunaipa',
-        description: 'A web application for the Akunaipa project, a non-profit organization that provides education and health services to the people of the country.',
-        image: Akunaipa,
-        link: 'https://akunaipa.com.co/',
-        linkText: 'Visit the website',
-    },
-    {
-        title: 'LandingPage',
-        description: 'A web application for the Akunaipa project, a non-profit organization that provides education and health services to the people of the country.',
-        image: LandingPageOne,
-        link: 'https://neiderruiz.github.io/landing-page/',
-        linkText: 'Visit the website',
-    },
-];
+type PropsProject = {
+    id: number;
+    index: number;
+    name: string;
+    description: string;
+    image: string;
+    url: string;
+    linkText: string;
+    short_description: string;
+    tecnologies: string;
+    openModal: () => void;
+};
 
 const Home: FC = () => {
+    const [projects, setProjects] = React.useState<PropsProject[]>([]);
+    const [isOpen, setIsOpen] = React.useState<number | boolean>(false);
+    const [modal, setModal] = React.useState({
+        name: '',
+        description: '',
+        image: '',
+        onClose: () => { },
+    });
+
+    useEffect(() => {
+        get('pages').then(res => {
+            if (res?.content) {
+                setProjects(res.content);
+            }
+        });
+    }, []);
+
+
+    const openModal = (id: number) => {
+        const project = projects.find(p => p.id === id);
+        if (project) {
+            setModal({
+                ...project,
+                onClose: () => setIsOpen(false),
+            });
+            setIsOpen(id);
+        }
+    }
+
     return (
         <Container>
-            <HeroHome />
-            <TitleSection text="Projects" />
-            <div className="space-y-14">
+            <HeroHome {...informationHome} showButtons />
+            <TitleSection text="Projectos" />
+            {
+                isOpen && <ModalMorePage {...modal} onClose={() => setIsOpen(false)} />
+            }
+            <div className="space-y-14" id="projects">
                 {
-                    projects.map((project, index) => (
-                        <Card {...project} key={index} />
+                    projects.map((project) => (
+                        <Card  {...project} key={project.id} openModal={() => openModal(project?.id)} />
+                    ))
+                }
+            </div>
+            <TitleSection text="Informaciòn de contacto" />
+            <div className="lg:flex lg:justify-center content-center cursor-pointer lg:space-x-5  lg:space-y-0 space-y-8 lg:px-40">
+                {
+                    informationContact.map((information, index) => (
+                        <CardContact key={index} {...information} />
                     ))
                 }
             </div>
