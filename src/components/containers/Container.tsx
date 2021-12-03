0ddfde23efe924db2d73f530769ecc03ca6e311d
neiderbtc <neiderbtc@gmail.com>
2021-12-03 09:39:50 -0500

    add useTitle page

--- src/components/containers/Container.tsx
+++ src/components/containers/Container.tsx
@@ -1,12 +1,15 @@
 import React, {FC} from 'react';
+import useTitle from '../../hooks/useTitle';
 import Footer from '../footers/Footer';
 import Nav from '../navs/Nav';
 type Props = {
     children: React.ReactNode;
+    title?: string;
 };
 
 
-const Container: FC<Props> = ({children}) => {
+const Container: FC<Props> = ({children, title}) => {
+    useTitle(title || 'Portafolio');
     return (
         <div className="bg-background">
             <Nav />
