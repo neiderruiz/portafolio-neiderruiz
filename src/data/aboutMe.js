5392f941dc0d7d9f3ad38377be84ff1930e219f5
neiderbtc <neiderbtc@gmail.com>
2021-12-03 09:43:42 -0500

    export information basic me

---
+++ src/data/aboutMe.js
@@ -0,0 +1,34 @@
+export const informationHome = {
+    title: 'Hola,  Mi nombre es Neider Ruiz',
+    subtitle: 'Desarrollador web',
+    description: `<p class="text-gray-800 text-2xl">
+    Soy un apasionado por la programación, con muchas ganas de mejorar y aprender día tras día con el objetivo de aportar valor a mi equipo de trabajo, ser mejor persona y ser mejor en mi profesión.
+    </p>`
+}
+
+export const informationAbout = {
+    title: 'Hola,  Mi nombre es Neider Ruiz',
+    subtitle: 'Desarrollador web',
+    description: `
+    <p class="text-lg">
+        Soy <b>Neider Ruiz</b>, tengo <b>21</b> vivo en la ciudad de <b>Bogotá - Colombia</b>,
+        me encanta programar y aprender nuevas tecnologías.
+    </p>
+    <p class="text-lg">
+        Comence en el mundo de la programación en el año <b>2018</b>, como muchos de nosotros con una motivación de aprender y crecer.
+    </p>
+    <p class="text-lg">
+        Aun sigo con esa emoción como cuando escribi mi primer <b>"Hola Mundo"</b>.
+    </p>`
+}
+
+export const informationContact = [
+    {
+        title: 'Correo Electronico',
+        description: 'neiderbtc@gmailcom',
+    },
+    {
+        title: 'Telefono',
+        description: '+57 318 597 0802',
+    },
+];
