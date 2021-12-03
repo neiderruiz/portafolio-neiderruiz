8c552107f579018b028b1e2bd73658ca82268e55
neiderbtc <neiderbtc@gmail.com>
2021-12-03 09:44:25 -0500

    add get fetch data api

---
+++ src/services/api/api.js
@@ -0,0 +1,12 @@
+const URL_API = process.env.REACT_APP_API_URL;
+export const get = (url) => {
+    return fetch(`${URL_API}${url}`, {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        },
+    })
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
