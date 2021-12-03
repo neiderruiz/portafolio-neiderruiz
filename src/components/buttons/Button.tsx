67c713ab44a4a286498c5bf58e34995e03c5186e
neiderbtc <neiderbtc@gmail.com>
2021-12-03 09:36:14 -0500

    remove link and add button, and action click

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,34 +1,23 @@
 import { FC } from "react";
-import { NavLink } from "react-router-dom";
 
 type Props = {
     text: string;
-    to?: string;
+    onClick?: () => void;
     type?: string;
     external?: boolean;
 };
-const Button: FC<Props> = ({ text, to, type, external }) => {
+const Button: FC<Props> = ({ text, onClick, type, external }) => {
     let typeButton: string;
     switch (type) {
         case 'secondary':
-            typeButton = 'border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-white text-white py-2 px-4 border-2';
+            typeButton = 'border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-white text-white py-2  border-2 ';
             break;
         default:
-            typeButton = 'bg-purple-600 hover:bg-purple-700 text-white';
+            typeButton = 'gradient-primary-button hover:gradient-primary-button  text-white py-2';
             break;
     }
 
-    return (
-        <div>
-        {
-            external ?
-            <a href={to} target="_blank" rel="noreferrer" className={`${typeButton} button py-3 px-6 rounded-lg font-bold transition delay-150 duration-300 ease-in-out`}>{text}</a>
-            :
-            <NavLink target="_blank" rel="noopener noreferrer" to={to || '/'} className={`${typeButton} button py-3 px-6 rounded-lg font-bold transition delay-150 duration-300 ease-in-out`}>
-                {text}
-            </NavLink>
-            }
-        </div>
+    return (<button onClick={onClick} className={`${typeButton} cursor-pointer px-6 rounded-lg font-bold transition delay-150 duration-300 ease-in-out`}>{text}</button>
     );
 };
 
