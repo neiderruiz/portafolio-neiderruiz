9b594cf2de7a8e8a745952b1999c6a87d2e11cfc
neiderbtc <neiderbtc@gmail.com>
2021-12-03 09:39:14 -0500

    add tags tecnologies, add button view more

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,26 +1,43 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import Button from '../buttons/Button';
+import ButtonLink from '../buttons/ButtonLink';
 
 type Props = {
-    title: string;
-    description: string;
+    name: string;
     image: string;
-    link: string;
+    url: string;
     linkText: string;
+    index: number;
+    short_description: string;
+    tecnologies: string;
+    openModal: () => void;
 };
 
-const Card: FC<Props> = ({title, description, image, link, linkText}) => {
+
+
+const Card: FC<Props> = ({ name, image, url, linkText, index, short_description, tecnologies, openModal }) => {
+
     return (
-        <div className="lg:w-8/12 h-auto mx-auto lg:grid lg:grid-cols-2 bg-white rounded-2xl shadow-sm flex flex-wrap-reverse">
-            <div className="my-auto space-y-5 px-10">                
-                    <h3 className="text-2xl text-purple-600 font-bold">{title}</h3>
-                    <p className="">{description}</p>
-                    <div>
-                    <Button external type="secondary" text="Ver web" to={link} />
-                    </div>
+        <div className={`   lg:w-8/12 h-auto mx-auto bg-white rounded-2xl shadow-sm flex ${index % 2 ? `lg:flex-row-reverse` : `lg:flex-row`}  flex-wrap-reverse hover:shadow-xl`}>
+            <div className="lg:my-auto space-y-5 lg:px-10 px-5 lg:w-3/6 my-10">
+                <h3 className="text-2xl gradient-text-primary font-bold">{name}</h3>
+                <p className="">{short_description}</p>
+               <div className="space-y-7">
+               <div>
+                    {
+                        tecnologies.split(',').map((item, index) =>
+                            <span className="inline-block bg-primary-light rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 bg-indigo-200 my-1">{item}</span>
+                        )
+                    }
+                </div>
+                <div className="flex justify-start space-x-5">
+                    <Button external text="Ver mas.." onClick={openModal} />
+                    <ButtonLink external type="secondary" text="Ver web" to={url} />
+                </div>
+               </div>
             </div>
-            <div>
-                <img className="lg:h-72 w-full" src={image} alt="lo" />
+            <div className="lg:w-3/6 cursor-pointer hover:rounded-lg ">
+                <img className="lg:h-82 w-full" src={image} alt={name} title={name} />
             </div>
         </div>
     );
